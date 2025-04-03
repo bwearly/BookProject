ab8062f770370a3a48f7c6039db9443e9be8237e
bwearly <153124661+bwearly@users.noreply.github.com>
2025-04-02 19:14:53 -0600

    fixed origins in cors

--- backend/Mission_Early.API/Program.cs
+++ backend/Mission_Early.API/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddDbContext<BookStoreContext>(options =>
 
 builder.Services.AddCors(options =>
     options.AddPolicy("AddReactApp", policy => {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins("http://localhost:3000", "https://book-early-backend-b6dxa5hbcxcvhfbu.eastus-01.azurewebsites.net/Book")
         .AllowAnyMethod()
         .AllowAnyHeader();
     }));
