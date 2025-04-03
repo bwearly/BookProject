e8314d388e789342e0b826dd456f15b725738dfb
bwearly <153124661+bwearly@users.noreply.github.com>
2025-04-02 19:10:21 -0600

    updated the categories filter

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:5000/Book/GetBookTypes'
+          'https://book-early-backend-b6dxa5hbcxcvhfbu.eastus-01.azurewebsites.net/Book'
         );
         const data = await response.json();
         setCategories(data);
